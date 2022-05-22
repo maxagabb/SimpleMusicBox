1e9fac9c4120675b0b64de0dffaf632d52b77917
maxagabb <maxagabb@gmail.com>
2022-05-22 17:02:57 -0400

    use pause indicator icon

--- mpv/scripts/dmenu-playlist.lua
+++ mpv/scripts/dmenu-playlist.lua
@@ -72,7 +72,9 @@ end
 mp.commandv("script-message", "osc-visibility", "never", "no-osd")
 if opts.music_osc ~= "" then
   local ov = mp.create_osd_overlay('ass-events')
-  ov.data = [[{\fs100\an5}]] .. "ll"
+  ov.data = [[{\an5\p1\alpha&FF\1c&H0000&\3a&Hff}]] ..
+      [[m30 15 b37 8 112 8 117 15 b125 22 125 75 118 82 b112 89 38 89 30 82 b23 75 23 22 30 15 m76 108]] ..
+      [[{\alpha&H79\1c&Hffffff&\3a&Hff} m-45 -25 l 2 2 l -45 28{\p0}]]
   mp.observe_property("pause", "bool", function(_, paused)
     if paused then ov:update()
     else ov:remove() end
