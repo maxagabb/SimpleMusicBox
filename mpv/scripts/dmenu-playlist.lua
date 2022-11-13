79db64b1d9ce558e5522e6f1400815afaddf43b5
maxagabb <maxagabb@gmail.com>
2022-11-13 11:08:31 -0500

    fix pause icon

--- mpv/scripts/dmenu-playlist.lua
+++ mpv/scripts/dmenu-playlist.lua
@@ -74,10 +74,13 @@ if opts.music_osc ~= "" then
   local ov = mp.create_osd_overlay('ass-events')
   ov.data = [[{\an5\p1\alpha&FF\1c&H0000&\3a&Hff}]] ..
       [[m30 15 b37 8 112 8 117 15 b125 22 125 75 118 82 b112 89 38 89 30 82 b23 75 23 22 30 15 m76 108]] ..
-      [[{\alpha&H79\1c&Hffffff&\3a&Hff} m-45 -25 l 2 2 l -45 28{\p0}]]
+      [[{\alpha&H39\1c&H0000ff&\3a&Hff} m-45 -25 l 2 2 l -45 28{\p0}]]
   mp.observe_property("pause", "bool", function(_, paused)
-    if paused then ov:update()
-    else ov:remove() end
+    mp.add_timeout(100 / 1000, function()
+      if paused then ov:update()
+      else ov:remove() end
+      --[[ mp.osd_message("pause: " .. (v and "yes" or "no")) ]]
+    end)
   end)
 end
 
